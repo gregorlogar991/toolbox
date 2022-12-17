082a87843087c420417d3a64763ade39229f3ff9
Gregor Logar <gregorlogar991@gmail.com>
2022-12-17 14:50:03 +0100

    Fix

--- install.sh
+++ install.sh
@@ -4,25 +4,19 @@ set -euo pipefail
 
 apt-get update
 apt-get upgrade -y
-apt-get install -y ca-certificates vim dnsutils strace ngrep htop ncdu git dstat net-tools curl wget iputils-ping tar unzip zip software-properties-common golang-go sudo
+apt-get install -y ca-certificates vim dnsutils strace ngrep htop ncdu git dstat net-tools curl wget iputils-ping tar unzip zip software-properties-common golang-go sudo exa bat
 
 
 ARCH=`uname -m`
 if [ "$ARCH" == "x86_64" ]; then
     TF_ARCH="linux_amd64"
-    EXA_ARCH="x86_64"
 elif [ "$ARCH" == "aarch64" ]; then
     TF_ARCH="darwin_arm64"
-    EXA_ARCH="armv7"
 else
     echo "Unknown arch"
     exit 1
 fi
 
-#TODO: EXA
-#EXA_VER=$(curl -s "https://api.github.com/repos/ogham/exa/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
-#curl -Lo exa.zip "https://github.com/ogham/exa/releases/latest/download/exa-linux-${EXA_ARCH}-v${EXA_VER}.zip"
-#unzip -q exa.zip bin/exa -d /usr/local
 #TF
 TER_VER=`curl -s https://api.github.com/repos/hashicorp/terraform/releases/latest | grep tag_name | cut -d: -f2 | tr -d \"\,\v | awk '{$1=$1};1'`
 wget https://releases.hashicorp.com/terraform/${TER_VER}/terraform_${TER_VER}_${TF_ARCH}.zip
